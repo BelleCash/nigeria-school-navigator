17688e8692c33c578406a3bc5c1c149d64b4605c
Lovina N. Ndukwe <68515809+BelleCash@users.noreply.github.com>
2026-04-28 16:32:15 +0100

    Update filter.js to small workable filter

--- Phase-2-Logic-Data/src/logic/filter.js
+++ Phase-2-Logic-Data/src/logic/filter.js
@@ -1,1 +1,15 @@
+// src/logic/filter.js
 
+export function filterSchools(data, { state, level, ownership, query }) {
+  return data.filter((school) => {
+    const matchState = state ? school.state === state : true;
+    const matchLevel = level ? school.education_level === level : true;
+    const matchOwnership = ownership ? school.ownership_type === ownership : true;
+
+    const matchQuery = query
+      ? school.school_name.toLowerCase().includes(query.toLowerCase())
+      : true;
+
+    return matchState && matchLevel && matchOwnership && matchQuery;
+  });
+}
